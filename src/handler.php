9067eebcae2037498548883780648c807ceb0b19
joascode <joaskramer@outlook.com>
2018-06-13 13:10:54 +0200

    Deleted CORS shit

--- src/handler.php
+++ src/handler.php
@@ -1,11 +1,11 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
-//header('Access-Control-Allow-Origin: http://192.168.99.100:8080');
-header('Access-Control-Allow-Methods: POST');
-//header("Access-Control-Allow-Headers: X-Requested-With");
-header("Access-Control-Allow-Credentials: true");
-header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Headers: Content-Type');
+// //header('Access-Control-Allow-Origin: http://192.168.99.100:8080');
+// header('Access-Control-Allow-Methods: POST');
+// //header("Access-Control-Allow-Headers: X-Requested-With");
+// header("Access-Control-Allow-Credentials: true");
+// header('Content-Type: application/json');
 
 date_default_timezone_set("Europe/Amsterdam");
 require_once 'vendor/autoload.php';
